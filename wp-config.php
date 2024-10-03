a4839f7bb8824f31f28fb395aab690e46195e8ad
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-10-03 23:36:16 +0700

    added database online

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'nhom2' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'avnadmin' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '__' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mysql-1e661f21-mail-b340.k.aivencloud.com:13876' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
