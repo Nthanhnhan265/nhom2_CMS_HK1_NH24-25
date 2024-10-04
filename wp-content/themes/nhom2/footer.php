1230fdeb2968198a8fb0aa28b26723b49254a05b
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-10-04 22:13:06 +0700

    added tailwdin cdn for development

---
+++ wp-content/themes/nhom2/footer.php
@@ -0,0 +1,8 @@
+  <footer>
+    footer
+  </footer>
+  <script src="<?php echo get_template_directory_uri(); ?>/build/index.js"></script>
+
+</body>
+<?php wp_footer() ?>
+</html>
---
+++ wp-content/themes/nhom2/index.php
@@ -0,0 +1,9 @@
+<?php
+  get_header('header.php'); 
+  ?>
+<h1 class="bg-red-500">page content</h1>
+
+<?php 
+  get_footer()
+
+?> 
