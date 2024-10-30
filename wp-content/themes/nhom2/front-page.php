778ab7d7c47338bb683f59046b3bddd29d92cd6b
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-10-30 07:39:34 +0700

    update: plugins

---
+++ wp-content/themes/nhom2/front-page.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Trang chủ    
+ */
+?>
+<?php get_header('header.php') ?>
+<h1>trang chu o day</h1>
+<?php get_footer() ?>
---
+++ wp-content/themes/nhom2/page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <div><?php the_content(); ?></div>
+    <?php endwhile; ?>
+<?php else : ?>
+    <p>No posts found.</p>
+<?php endif; ?>
+<?php get_footer() ?>
+<?php
+
+?>
